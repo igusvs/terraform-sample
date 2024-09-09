3076a1173a0dd2966be9f4df44d417f304cd9c5a
igusvs <igoralves159@gmail.com>
2024-09-08 21:06:33 -0300

    ignore

---
+++ 01-terraform-basico/01-primeiro-script/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 0.12"
+
+  required_providers {
+    aws {
+      source = "hashicorpo/aws"
+      version = "5.66.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "igu-bucket-terraform-01"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+  }
+}
--- main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_s3_bucket" "bucketzao" {
-  bucket = "bucket-do-igu-123"
-  tags   = var.default_tags
-}
